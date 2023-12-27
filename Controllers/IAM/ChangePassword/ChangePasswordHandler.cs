3db204ae102ad5c0e7ae7f6ccc7337e491a75944
vdae2304 <vdae_2304@hotmail.com>
2023-12-27 14:58:03 -0600

    Endpoint para cambio y recuperación de contraseña

---
+++ Controllers/IAM/ChangePassword/ChangePasswordHandler.cs
@@ -0,0 +1,76 @@
+﻿using Ecommerce.Common.Exceptions;
+using Ecommerce.Common.Models.IAM;
+using Ecommerce.Common.Models.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Controllers.IAM.ChangePassword
+{
+    public record ChangePasswordRequest : IRequest<IActionResult>
+    {
+        /// <summary>
+        /// User ID.
+        /// </summary>
+        [BindNever]
+        [FromRoute]
+        public int UserId { get; set; }
+        /// <summary>
+        /// Current password.
+        /// </summary>
+        [Required]
+        public string CurrentPassword { get; set; } = string.Empty;
+        /// <summary>
+        /// New password.
+        /// </summary>
+        [Required]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+
+    public class ChangePasswordHandler : IRequestHandler<ChangePasswordRequest, IActionResult>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<ChangePasswordHandler> _logger;
+
+        public ChangePasswordHandler(UserManager<ApplicationUser> userManager, ILogger<ChangePasswordHandler> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Handle(ChangePasswordRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var validator = new ChangePasswordValidator();
+                var validationResult = validator.Validate(request);
+                if (!validationResult.IsValid)
+                {
+                    throw new BadRequestException(validationResult.ToString());
+                }
+
+                ApplicationUser user = await _userManager.FindByIdAsync(request.UserId.ToString())
+                    ?? throw new UnauthorizedException("Your username or password is incorrect");
+
+                var result = await _userManager.ChangePasswordAsync(user, request.CurrentPassword, request.NewPassword);
+                if (!result.Succeeded)
+                {
+                    throw new UnauthorizedException("Your username or password is incorrect");
+                }
+
+                return new OkObjectResult(new Response
+                {
+                    Success = true,
+                    Message = "Ok."
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in changing password for {userId}", request.UserId);
+                throw;
+            }
+        }
+    }
+}
---
+++ Controllers/IAM/ChangePassword/ResetPasswordHandler.cs
@@ -0,0 +1,75 @@
+﻿using Ecommerce.Common.Exceptions;
+using Ecommerce.Common.Models.IAM;
+using Ecommerce.Common.Models.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Controllers.IAM.ChangePassword
+{
+    public record ResetPasswordRequest : IRequest<IActionResult>
+    {
+        /// <summary>
+        /// Email address.
+        /// </summary>
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        /// <summary>
+        /// New password.
+        /// </summary>
+        [Required]
+        public string NewPassword { get; set; } = string.Empty;
+        /// <summary>
+        /// Password reset token.
+        /// </summary>
+        [Required]
+        public string Token { get; set; } = string.Empty;
+    }
+
+    public class ResetPasswordHandler : IRequestHandler<ResetPasswordRequest, IActionResult>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<ResetPasswordHandler> _logger;
+
+        public ResetPasswordHandler(UserManager<ApplicationUser> userManager, ILogger<ResetPasswordHandler> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Handle(ResetPasswordRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var validator = new ResetPasswordValidator();
+                var validationResult = validator.Validate(request);
+                if (!validationResult.IsValid)
+                {
+                    throw new BadRequestException(validationResult.ToString());
+                }
+
+                ApplicationUser? user = await _userManager.FindByEmailAsync(request.Email);
+                if (user != null)
+                {
+                    var result = await _userManager.ResetPasswordAsync(user, request.Token, request.NewPassword);
+                    if (!result.Succeeded)
+                    {
+                        throw new BadRequestException("Invalid token");
+                    }
+                }
+
+                return new OkObjectResult(new Response
+                {
+                    Success = true,
+                    Message = "Ok."
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in changing password for {email}", request.Email);
+                throw;
+            }
+        }
+    }
+}
